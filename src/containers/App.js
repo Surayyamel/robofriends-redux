e847aaf7c10b88c873733ff439836ea5e3e94b8f
Surayya <Surayyamel@users.noreply.github.com>
2020-03-25 13:37:24 +0100

    cleaned didMount function

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,15 +14,22 @@ class App extends Component {
 		}
 	}
 
+// componentDidMount() {
+// 	fetch('http://jsonplaceholder.typicode.com/users')
+// 		.then(response=> {
+// 		  return response.json();
+// 		})
+// 		.then(users=> {
+// 			this.setState({ robots: users})
+// 		})
+// }
+
 componentDidMount() {
-	fetch('http://jsonplaceholder.typicode.com/users')
-		.then(response=> {
-		  return response.json();
-		})
-		.then(users=> {
-			this.setState({ robots: users})
-		})
-}
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+
 
 
 
