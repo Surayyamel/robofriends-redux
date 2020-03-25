c34cd018ce3a790510bd64c209238129ddf12c77
Surayya <Surayyamel@users.noreply.github.com>
2020-03-25 12:42:05 +0100

    tried to fix loading h1 in App

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,25 +31,23 @@ onSearchChange = (event) => {
 	})
 }
 
-render() {
-	const { robots, searchfield } = this.state;
-	const filteredRobots = robots.filter(robot => {
-	return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-})
-	return !robots.length ?
-		<h1>Loading</h1> :
-	  (
-		<div className='tc'>
-		  	<h1 className='f1'>RoboFriends</h1>
-		  	<SearchBox searchChange={this.onSearchChange} />
-		  	<Scroll>
-		  		<CardList robots={filteredRobots} />
-		  	</Scroll>
-		</div>
-
-  		);
-	}
-
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
 
-  export default App;
+export default App;
